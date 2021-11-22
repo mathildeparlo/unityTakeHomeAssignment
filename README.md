85aed8da2d1ece45be00a07ed18f539008e72d66
Mathilde Parlo <minthilde@gmail.com>
2021-11-22 08:51:52 +0100

    job.fsx added

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
  unityTakeHomeAssignment
+
+Navigate to the directory of job.fsx in a mono terminal
+Run the code in mono with the following command:
+
+$ fsharpc job.fsx && mono job.exe
---
+++ job.fsx
@@ -0,0 +1,93 @@
+module jobQueue
+
+open System
+open System.Threading
+
+let now = DateTime.Now
+
+/// <summary>                   A class representing a job                                  </summary>
+/// <param name="Id">           An integer that uniquely identifies a job                   </param>
+/// <param name="Duration">     The amount of seconds the job takes to run (rep. as int)    </param>
+/// <param name="HasPriority">  When true, the job takes precedence over non-priority jobs  </param>
+/// <param name="Submitted">    A date/time for when the job was submitted                  </param> 
+type Job (Id: int, Duration: int, HasPriority: bool, Submitted: DateTime) =
+  
+  member this.Id = Id
+  member this.Duration = Duration
+  member this.HasPriority = HasPriority
+  member this.Submitted = Submitted
+
+
+let mutable jobQueue = []
+
+/// <summary>             Schedules a given job by adding it to a list  </summary>
+/// <param name="job">    The job to add to the queue                   </param>
+/// <returns>             Updates global variable queue, returns a unit </returns>
+let scheduleJob (job : Job) : unit =
+
+  jobQueue <- jobQueue @ [job]
+
+
+/// <summary>           Helper function that dermines how to use List.sortWith  </summary>
+/// <param name="job1"> First job for comparison                                </param>
+/// <param name="job2"> Second job for comparison                               </param>
+/// <returns>           An int indication which job takes precedence            </returns>
+let timeCompare (job1: Job) (job2: Job) : int =
+  
+  if job1.HasPriority && not job2.HasPriority then
+    -1
+  elif job2.HasPriority && not job1.HasPriority then
+    1
+  else
+    DateTime.Compare (job1.Submitted, job2.Submitted)
+
+
+/// <summary>             Picks the next appropriate job and runs it    </summary>
+/// <param name="queue">  A list of tupples each representing a job     </param>
+/// <returns>             A print statement emulating the run of a job  </returns>
+let runNextJob (queue : list<Job>) : unit =
+
+  let timeSortedQueue = queue |> List.sortWith timeCompare
+
+  let mutable tempQueue = []
+
+  let rec NextJob (q: list<Job>) : unit = 
+
+    match q with
+      | h::t -> if DateTime.Compare (h.Submitted, now.AddSeconds -5.0) <> 1 then
+                  jobQueue <- tempQueue @ t
+                  printfn "\nRunning job %A and waiting %A seconds\n" h.Id h.Duration
+                  Thread.Sleep(h.Duration * 1000)
+
+                elif h.HasPriority then
+                  tempQueue <- tempQueue @ [h]
+                  NextJob t
+                else
+                  jobQueue <- tempQueue @ q
+                  printfn "\nThere was no job in the queue older than 5 sec. Try again soon :-)"
+
+      | [] -> printfn "\nThere are no scheduled jobs"
+
+  NextJob timeSortedQueue
+
+
+
+/// ----main----
+
+/// Creating jobs
+let job1 = Job(1, 3, true, new DateTime (2021, 11, 20, 12, 12, 1))
+let job2 = Job(2, 2, false, new DateTime (2021, 11, 20, 12, 12, 10))
+let job3 = Job(3, 5, false, new DateTime (2021, 11, 20, 12, 12, 3))
+let job4 = Job(4, 4, false, new DateTime (2021, 11, 20, 12, 12, 4))
+let job5 = Job(5, 1, true, new DateTime (2021, 11, 20, 12, 12, 5))
+
+// Scheduling jobs for the JobQueue
+scheduleJob job1
+scheduleJob job2
+scheduleJob job3
+scheduleJob job4
+scheduleJob job5
+
+// Running the job queue
+for i in 0..jobQueue.Length - 1 do
+  runNextJob jobQueue
